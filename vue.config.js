1234a1a68a6a13cb07afa7ad53804b5a198cce76
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-12-24 18:49:38 +0900

    vue config 수정

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/seungheon123/'  // GitHub Pages 경우
-    : '/',
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   transpileDependencies: true
-}
+})
